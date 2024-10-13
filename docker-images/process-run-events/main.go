83c8633f997544a901f32434f8b36a2c38eaf2e0
Thomas Briot <thomas.briot82@gmail.com>
2024-10-13 11:45:20 -0400

    feat: container is reading msg from sqs

--- docker-images/process-run-events/main.go
+++ docker-images/process-run-events/main.go
@@ -1,9 +1,57 @@
 package main
 
 import (
+	"context"
 	"log"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/sqs"
+	"github.com/aws/aws-sdk-go-v2/service/sqs/types"
 )
 
+const QUEUE_URL = "https://sqs.ca-central-1.amazonaws.com/253789223556/tfc-run-events"
+
+// SqsActions encapsulates the Amazon Simple Queue Service (Amazon SQS) actions
+type SqsActions struct {
+	SqsClient *sqs.Client
+}
+
+func newSqsActions() *SqsActions {
+	cfg, err := config.LoadDefaultConfig(context.TODO(),
+		config.WithRegion("ca-central-1"),
+	)
+	if err != nil {
+		log.Fatalf("unable to load SDK config, %v", err)
+	}
+
+	return &SqsActions{SqsClient: sqs.NewFromConfig(cfg)}
+}
+
+// GetMessages uses the ReceiveMessage action to get messages from an Amazon SQS queue.
+func (actor SqsActions) GetMessages(ctx context.Context, queueUrl string, maxMessages int32, waitTime int32) ([]types.Message, error) {
+	var messages []types.Message
+	result, err := actor.SqsClient.ReceiveMessage(ctx, &sqs.ReceiveMessageInput{
+		QueueUrl:            aws.String(queueUrl),
+		MaxNumberOfMessages: maxMessages,
+		WaitTimeSeconds:     waitTime,
+	})
+	if err != nil {
+		log.Printf("Couldn't get messages from queue %v. Here's why: %v\n", queueUrl, err)
+	} else {
+		messages = result.Messages
+	}
+	return messages, err
+}
+
 func main() {
-	log.Println("Hello world!")
+	sqsActions := newSqsActions()
+	for {
+		messages, _ := sqsActions.GetMessages(context.TODO(), QUEUE_URL, 2, 5)
+
+		log.Printf("Received %d messages", len(messages))
+		for _, msg := range messages {
+			log.Printf("Received message with ID=%v, Body=%v", *msg.MessageId, *msg.Body)
+		}
+	}
 }
