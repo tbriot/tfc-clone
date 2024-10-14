c6cf5fb56b6c151beb2dec84c99f5751abb2d278
Thomas Briot <thomas.briot82@gmail.com>
2024-10-13 20:36:28 -0400

    feat: cache tf binaries on efs

--- docker-images/process-run-events/main.go
+++ docker-images/process-run-events/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"log"
+	"os"
+	"os/exec"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
@@ -11,6 +13,7 @@ import (
 )
 
 const QUEUE_URL = "https://sqs.ca-central-1.amazonaws.com/253789223556/tfc-run-events"
+const CACHE_MOUNPOINT = "/opt/tfc-cache"
 
 // SqsActions encapsulates the Amazon Simple Queue Service (Amazon SQS) actions
 type SqsActions struct {
@@ -44,7 +47,70 @@ func (actor SqsActions) GetMessages(ctx context.Context, queueUrl string, maxMes
 	return messages, err
 }
 
+func switchTfVersion(version string, cache bool) {
+	var cmd *exec.Cmd
+	if cache {
+		cmd = exec.Command("tfswitch", "-i", CACHE_MOUNPOINT+"/terraform", version)
+	} else {
+		cmd = exec.Command("tfswitch", version)
+	}
+	stdout, err := cmd.Output()
+
+	if err != nil {
+		log.Println("tfswitch. error:" + err.Error())
+	}
+
+	log.Println("tfswitch. stdout:" + string(stdout))
+}
+
+func displayTfVersion() {
+	app := "terraform"
+	arg0 := "-version"
+
+	cmd := exec.Command(app, arg0)
+	stdout, err := cmd.Output()
+
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	// Print the output
+	log.Println("displayTfVersion:" + string(stdout))
+}
+
+func listDir(path string) {
+	log.Printf("Listing content of dir=%v", path)
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, e := range entries {
+		log.Println(e.Name())
+	}
+}
+
 func main() {
+	listDir(CACHE_MOUNPOINT + "/terraform/.terraform.versions")
+	// tfswitch to 1.9.7 using custom install dir
+	switchTfVersion("1.9.7", true)
+	displayTfVersion()
+	// tfswitch to 1.9.7 using custom install dir
+	switchTfVersion("1.9.7", false)
+	displayTfVersion()
+	// tfswitch to 1.8.5 custom install dir
+	switchTfVersion("1.8.5", true)
+	displayTfVersion()
+	// tfswitch to 1.8.5 custom install dir
+	switchTfVersion("1.8.5", false)
+	displayTfVersion()
+	// tfswitch to 1.8.3 custom install dir
+	switchTfVersion("1.8.3", true)
+	displayTfVersion()
+	// tfswitch to 1.8.3 custom install dir
+	switchTfVersion("1.8.3", false)
+	displayTfVersion()
+
 	sqsActions := newSqsActions()
 	for {
 		messages, _ := sqsActions.GetMessages(context.TODO(), QUEUE_URL, 2, 5)
